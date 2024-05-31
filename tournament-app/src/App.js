9c75e3a76f9dab25ad36262206f9e81ecf267a2a
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2024-05-31 15:11:08 -0230

    First Commit

---
+++ tournament-app/src/App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import PlayerInput from './components/PlayerInput';
+import TeamDisplay from './components/TeamDisplay';
+import TournamentSchedule from './components/TournamentSchedule';
+import ResultsInput from './components/ResultsInput';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Backyard Games Tournament</h1>
+      <PlayerInput />
+      <TeamDisplay />
+      <TournamentSchedule />
+      <ResultsInput />
+    </div>
+  );
+}
+
+export default App;
+
